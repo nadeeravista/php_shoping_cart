78bce34a04b86eade39b7d2cfde18aeb010761d5
nadeera.vista@gmail.com <nadeera.vista@gmail.com>
2020-08-18 01:35:16 +1200

    DockBlocks added

--- src/Product.php
+++ src/Product.php
@@ -9,13 +9,28 @@ namespace App;
 class Product
 {
 
+    /**
+     *
+     * @var array Prduct list as array
+     */
     private $products;
 
+    /**
+     * Injecting dependancy of data to the class in object cration
+     *
+     * @param array $products Product list as array
+     */
     public function __construct($products)
     {
         $this->products = $products;
     }
 
+    /**
+     * Undocumented function
+     *
+     * @param string $name Name of the product
+     * @return array The product details as array
+     */
     public function getByName(string $name): array
     {
         $key = array_search($name, array_column($this->products, 'name'));
