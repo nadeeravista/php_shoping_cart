3a10e9dd290ee04769045305d751672a7dffd879
nadeera.vista@gmail.com <nadeera.vista@gmail.com>
2020-09-21 03:14:12 +1200

    Adding phpDoc blocks and adding a function to render json responses

--- src/facades/Response.php
+++ src/facades/Response.php
@@ -2,16 +2,29 @@
 
 namespace Nadeera\Facades;
 
+/**
+ * Represents handing the response genereated at the controller/action
+ * @author Nadeera
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
 class Response
 {
-    public static function view($file, $data)
+    /**
+     * This is a facade/helper used to render the view
+     *
+     * @param string $file
+     * @param mix $data
+     * @return void
+     */
+    public static function view(string $file, $data)
     {
-        $fileLocationParts = explode(".",$file);
-        $fileLocationParts [] = array_pop($fileLocationParts).".php";
+        $fileLocationParts = explode(".", $file);
+        $fileLocationParts[] = array_pop($fileLocationParts) . ".php";
         $filePath = implode("/", $fileLocationParts);
-        
-        if(is_array($data)){
-            foreach($data as $key=>$item){
+
+        if (is_array($data)) {
+            foreach ($data as $key => $item) {
                 $$key = $item;
             }
         } else {
@@ -19,5 +32,16 @@ class Response
         }
         include "./config.php";
         include $filePath;
+    }
+
+    /**
+     * Response will be renderd as array. Can be used when creating APIs
+     *
+     * @param array $data
+     * @return string
+     */
+    public static function json(array $data): string
+    {
+        return json_encode($data);
     }
 }
