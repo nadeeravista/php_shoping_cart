f5f929896921363057ebdfdd3527730dc1d155c2
nadeera.vista@gmail.com <nadeera.vista@gmail.com>
2020-09-21 04:21:13 +1200

    Adding config to index

--- index.php
+++ index.php
@@ -14,6 +14,11 @@ header( "Set-Cookie: name=value; httpOnly" );
 require_once realpath("vendor/autoload.php");
 
 /**
+ * Setting initial php settings
+ */
+include_once('./config.php');
+
+/**
  * This is a vanilla php app, thus the actions.php is acting as the global route file and the controller 
  */
 include_once('./actions.php');
