bb2aae05d735b8442cde7b77aa7a8e44b0f5f5c0
nadeera.vista@gmail.com <nadeera.vista@gmail.com>
2020-09-21 02:09:27 +1200

    Adding XSS safety and  moving view rendering to a Class

--- index.php
+++ index.php
@@ -5,19 +5,17 @@
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
 
-include "./config.php";
+
+ /**
+  * This header secure the application from XSS attacks
+  */
+header( "Set-Cookie: name=value; httpOnly" );
 
 require_once realpath("vendor/autoload.php");
 
 /**
- * This is a vanilla php app, thus the actions.php is the global route file and controller 
+ * This is a vanilla php app, thus the actions.php is acting as the global route file and the controller 
  */
 include_once('./actions.php');
-
-/**
- * This app has only a simple action route and one view, thus it is including directly rather 
- * render using a response class
- */
-include_once('./view.php');
 ?>
 
